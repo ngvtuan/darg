791090b9e8114132020c7607e1f87c50d8a6d27c
jirkaschaefer <jirka.schaefer@sherpany.com>
2015-04-18 10:43:18 +0100

    split settings to allow for better testing and static file handling

---
+++ site/project/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+from kombu import Exchange, Queue
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+MANAGERS = ADMINS
+
+# needed to send build screenshot of FE tests
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SESSION_COOKIE_NAME = 'vbnet-sessionid'
+
+TRACKING_ENABLED = False
+
+#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+# for local dev use this...
+#NOSE_ARGS = ['--pdb', '-s', '--logging-level=WARNING']
+
+#INSTALLED_APPS = INSTALLED_APPS + ('rosetta', 'django_nose',)

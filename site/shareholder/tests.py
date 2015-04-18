4fd96a7a0049fe148c1f19b433b3507535632057
jirkaschaefer <jirka.schaefer@sherpany.com>
2015-04-18 05:38:56 +0100

    added first test for index page

--- site/shareholder/tests.py
+++ site/shareholder/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.test.client import Client
+
+from django.test.client import RequestFactory
 
 # Create your tests here.
+class ShareholderTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+
+    def test_index(self):
+
+        response = self.client.get("/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Das Akt" in response.content)

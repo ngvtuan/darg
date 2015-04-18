c65f88b3aea5270c944274d33d1fbb09290040f3
jirkaschaefer <jirka.schaefer@sherpany.com>
2015-04-18 17:19:25 +0100

    resolve #24 added login_required decorator to authed views

--- site/project/views.py
+++ site/project/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     template = loader.get_template('index.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
 
+@login_required
 def start(request):
     template = loader.get_template('start.html')
     context = RequestContext(request, {})
--- site/shareholder/views.py
+++ site/shareholder/views.py
@@ -1,17 +1,25 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 
+from shareholder.models import Shareholder
+
+@login_required
 def positions(request):
     template = loader.get_template('positions.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
 
+@login_required
 def log(request):
     template = loader.get_template('log.html')
     context = RequestContext(request, {})
     return HttpResponse(template.render(context))
 
+@login_required
 def shareholder(request, shareholder_id):
     template = loader.get_template('shareholder.html')
-    context = RequestContext(request, {})
+    shareholder = get_object_or_404(Shareholder, id=int(shareholder_id))
+    context = RequestContext(request, {'shareholder': shareholder})
     return HttpResponse(template.render(context))

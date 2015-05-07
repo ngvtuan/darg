6aa88ebdc3c3612670b13a104b614a103581e0db
jirkaschaefer <jirka.schaefer@sherpany.com>
2015-05-07 19:12:57 +0100

    made dev settings use sqlite and not psql

--- site/project/settings/dev.py
+++ site/project/settings/dev.py
@@ -24,3 +24,10 @@ STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'static', 'minified'),
     os.path.join(BASE_DIR, 'static'),
 )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}

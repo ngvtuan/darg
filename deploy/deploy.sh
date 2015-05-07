660b8df20b941329e42a0eebdd3cfdc917340d0b
jirkaschaefer <jirka.schaefer@sherpany.com>
2015-05-07 01:12:19 +0100

    cleaned deployment code

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,6 @@ source ../.ve/bin/activate
 
 git pull
 pip install -r requirements.txt
-python ./minify_static.py
-./site/manage.py migrate 
-./site/manage.py collectstatic --noinput
+python ../minify_static.py
+../site/manage.py migrate 
+../site/manage.py collectstatic --noinput

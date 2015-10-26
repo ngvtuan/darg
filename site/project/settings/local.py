2bee33dfa10f2ed89d1f7582487c16acdea062f9
Jirka Schaefer <jirka@tschitschereengreen.com>
2015-10-26 06:41:32 +0100

    removed local settings

--- site/project/settings/local.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = '1t+&creo_(j$$fwomray@wdv$t=@4t@5y*#bhk5k50usf1)2ag'
-SITE_ID = 3
-RAVEN_CONFIG = {
-    'dsn': 'http://da49e25357134a48b97e9270622dd0cf:57c28c834d3d406b904257d05aca77ab@aggregator.ttg-dresden.de:9000/4',
-}

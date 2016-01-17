4f9b1c655f159646f873cd8e669d1a7ee1702c46
Jirka Schaefer <jirka@tschitschereengreen.com>
2016-01-17 08:47:21 +0100

    updated version at base.py to 0.1.20

--- site/project/settings/base.py
+++ site/project/settings/base.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-VERSION = '0.1.19'
+VERSION = '0.1.20'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 

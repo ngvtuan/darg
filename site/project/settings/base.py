c14f821086763c1f5103fb3d5a1a9ed86349aa48
Jirka Schaefer <jirka@tschitschereengreen.com>
2016-04-01 00:28:16 +0200

    enabled sor

--- site/project/settings/base.py
+++ site/project/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework.authtoken',
     'raven.contrib.django.raven_compat',
+    'sorl.thumbnail',
 
     # -- zinnia
     'django_comments',

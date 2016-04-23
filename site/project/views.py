8a1b929a4520c938ac5871b026e0512d0ee76088
Jirka Schaefer <jirka@tschitschereengreen.com>
2016-04-23 02:05:57 +0200

    even more stable csv with unicode

--- site/project/views.py
+++ site/project/views.py
@@ -49,14 +49,15 @@ def captable_csv(request, company_id):
         'shareholder number', 'last name', 'first name',
         'email', 'share count', 'share percent'])
     for shareholder in company.get_active_shareholders():
-        writer.writerow([
-            shareholder.number.encode('utf-8'),
-            shareholder.user.last_name.encode('utf-8'),
-            shareholder.user.first_name.encode('utf-8'),
-            shareholder.user.email.encode('utf-8'),
+        row = [
+            shareholder.number,
+            shareholder.user.last_name,
+            shareholder.user.first_name,
+            shareholder.user.email,
             shareholder.share_count(),
-            shareholder.share_percent().encode('utf-8')
-        ])
+            shareholder.share_percent(),
+        ]
+        writer.writerow([unicode(s).encode("utf-8") for s in row])
 
     return response
 

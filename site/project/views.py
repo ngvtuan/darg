5407a5ad43455ba634f3923077b5a3ff1a3e9c00
Jirka Schaefer <jirka@tschitschereengreen.com>
2016-05-09 23:57:35 +0200

    user from instapage import truncated at 29 chars

--- site/project/views.py
+++ site/project/views.py
@@ -60,7 +60,7 @@ def instapage(request):
 
     kwargs = dict(
         first_name=first_name, last_name=last_name, email=email,
-        is_active=True, username=email,
+        is_active=True, username=email[:29],
         )
 
     # save data

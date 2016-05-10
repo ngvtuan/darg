6fb90163ea8ddfd6e2add4cca7ac5122187ef376
Jirka Schaefer <jirka@tschitschereengreen.com>
2016-05-10 10:12:44 +0200

    additional test for username generation due to sentry error

--- site/utils/tests.py
+++ site/utils/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from utils.user import make_username
 
+
 class UtilsTestCase(TestCase):
 
     def test_make_username(self):
@@ -15,3 +16,20 @@ class UtilsTestCase(TestCase):
         self.assertTrue(len(username) > 0)
         self.assertTrue(len(username) < 30)
         self.assertTrue(isinstance(username, str))
+
+    def test_unique_username(self):
+        """
+        on prod we had
+        http://aggregator.ttg-dresden.de:9000/sentry-internal/production/
+        issues/28313/
+        jirka2@tschitschereengreen.com
+        vs
+        jirka+test2@kkd-partners.com
+        """
+        username1 = make_username(
+            'Jirka', 'Schaefer', u'jirka@tschitschereengreen.com')
+
+        username2 = make_username(
+            'Jirka2', 'Schaefer2', u'jirka2@tschitschereengreen.com')
+
+        self.assertNotEqual(username1, username2)

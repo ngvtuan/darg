1d632c3378213262895b5ef3f515cf241678005f
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2016-07-22 13:37:48 +0200

    re #87 added segment validation mixin for API

--- site/services/rest/mixins.py
+++ site/services/rest/mixins.py
@@ -14,13 +14,28 @@ class FieldValidationMixin(object):
 
     def validate_number_segments(self, value):
 
-        # validate value as we have to track value...
         pattern = re.compile(r'[^0-9,\- ]')
         for part in value:
+
+            # validate value as we have to track value...
             if pattern.findall(str(part)):
                 raise serializers.ValidationError(
                     _("Invalid number segment. "
                       "Please use 1, 2, 3, 4-10.")
                 )
                 logger.warning("Invalid number segment: {}".format(part))
+
+            if isinstance(part, int):
+                continue
+
+            # --- VALIDATE u'X-Z' only
+            # validate that start and end of u'1-9' are valid
+            start, end = part.split('-')
+            if int(start) >= int(end):
+                raise serializers.ValidationError(
+                    _("Number Segment Range start smaller/equal then end '{}'. "
+                      "Please use 1, 2, 3, 4-10.".format(part))
+                )
+                logger.warning("Invalid number segment: {}".format(part))
+
         return value

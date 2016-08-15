80b9e7b93ea6b310d3a5e6cfc8112e6be1be79a5
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2016-08-15 10:01:52 +0200

    updated version at base.py to 0.3.26

--- site/project/settings/base.py
+++ site/project/settings/base.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 from kombu import Exchange, Queue
 
-VERSION = '0.3.25'
+VERSION = '0.3.26'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 

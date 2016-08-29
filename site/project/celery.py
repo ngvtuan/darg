68dadfe50b5aac49e9f99395d8d0fabaaf413a9c
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2016-08-29 22:34:55 +0200

    config celery/raven to log stack locals

--- site/project/celery.py
+++ site/project/celery.py
@@ -5,14 +5,28 @@ from __future__ import absolute_import
 
 import os
 
-from celery import Celery
+import celery
+import raven
+from raven.contrib.celery import register_signal, register_logger_signal
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.base')
 
 from django.conf import settings  # noqa
 
-app = Celery('project')
+
+class Celery(celery.Celery):
+
+    def on_configure(self):
+        client = raven.Client(settings.RAVEN_CONFIG.get('dsn'))
+
+        # register a custom filter to filter out duplicate logs
+        register_logger_signal(client)
+
+        # hook into the Celery error handler
+        register_signal(client)
+
+app = Celery(__name__)
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)

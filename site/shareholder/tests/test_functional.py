de4a27deeb3c6235eeec62bf4a5bbcaf4f6f5d82
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2016-11-08 16:22:29 +0100

    fix date assert # 2

--- site/shareholder/tests/test_functional.py
+++ site/shareholder/tests/test_functional.py
@@ -555,7 +555,7 @@ class PositionFunctionalTestCase(BaseSeleniumTestCase):
 
             self.assertEqual(len(app.get_position_row_data()), 8)
             self.assertTrue(app.is_no_errors_displayed())
-            self.assertEqual(app.get_position_row_data()[0][:8],
+            self.assertEqual(app.get_position_row_data()[0].split('\n')[0],
                              datetime.datetime.today().strftime('%-d.%-m.%y'))
 
             app.refresh()

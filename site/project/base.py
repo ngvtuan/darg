9c2436fb37722ef22014957886769fd703c01339
Jirka Schaefer <jirka.schaefer@kkd-partners.com>
2016-11-08 16:45:11 +0100

    fix chromedriver path

--- site/project/base.py
+++ site/project/base.py
@@ -66,7 +66,7 @@ class BaseSeleniumTestCase(LiveServerTestCase):
         chrome_options = Options()
         chrome_options.add_argument("--start-maximized")
         cls.selenium = WebDriver(
-            os.path.join(os.getcwd(), 'site/chromedriver'),
+            os.path.join(os.getcwd(), 'chromedriver'),
             chrome_options=chrome_options)
 
         cls.selenium.implicitly_wait(10)
